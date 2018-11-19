b5b8ad2df8dc525aa5d1e3c9928d8bec15adab96
宝龙 <35037130+huanghuikang@users.noreply.github.com>
2018-11-19 23:35:46 +0800

    Update Two.sql

--- SQLServer/Two.sql
+++ SQLServer/Two.sql
@@ -1,1 +1,14 @@
+USE [ekp]
+GO
+/****** Object:  StoredProcedure [dbo].[bumenchaxunjiane]    Script Date: 11/07/2018 11:25:06 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[bumenchaxunjiane]
+	@fd_beginTime varchar(100) ,
+	@fd_endTime varchar(100),
+	@fdept varchar(255)
+AS
+BEGIN
 
