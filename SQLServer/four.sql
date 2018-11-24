e4076df534cdf091fa64c8b3f7e58a4debb12dd1
宝龙 <35037130+huanghuikang@users.noreply.github.com>
2018-11-24 23:55:12 +0800

    Update four.sql

--- SQLServer/four.sql
+++ SQLServer/four.sql
@@ -1,1 +1,15 @@
-
+USE [ekp]
+GO
+/****** Object:  StoredProcedure [dbo].[yufukuan_fukuan]    Script Date: 11/07/2018 11:27:50 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[yufukuan_fukuan]
+	--(@fd_faShengKaiShiShiJian datetime,@fd_faShengJieShuShiJian datetime)
+	(
+		@fd_beginTime varchar(100) ,
+		@fd_endTime varchar(100)
+	)
+AS
+BEGIN
