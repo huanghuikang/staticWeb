42f0377ac4712bcc06ad69d7175d5d5e372c3614
宝龙 <35037130+huanghuikang@users.noreply.github.com>
2018-11-24 23:55:44 +0800

    Update four.sql

--- SQLServer/four.sql
+++ SQLServer/four.sql
@@ -13,3 +13,24 @@ ALTER PROCEDURE [dbo].[yufukuan_fukuan]
 	)
 AS
 BEGIN
+	--DECLARE @fd_beginTime varchar(100),@fd_endTime varchar(100)
+	create table #tongjifeiyong(
+		forgid varchar(200),
+		ftype varchar(100),
+		fmount decimal(18,2),
+	)
+	insert #tongjifeiyong
+	select t1.fd_chengdanbumen,t1.fd_3651029d66ed78_text,SUM(t1.fd_fukuanjine) as fd_jinE from ekp_yufukuan t1
+	where 1=1
+		AND 1 = CASE WHEN ISNULL(@fd_beginTime, '') = ''  THEN 1
+		ELSE CASE WHEN t1.fd_shenqingriqi>=@fd_beginTime 
+				THEN 1
+				ELSE 0
+			END
+		END
+		AND 1 = CASE WHEN ISNULL(@fd_endTime, '') = ''  THEN 1
+		ELSE CASE WHEN t1.fd_shenqingriqi<=@fd_endTime 
+				THEN 1
+				ELSE 0
+			END
+		END
