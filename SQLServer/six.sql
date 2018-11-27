5986254602cb3f043fdcaf25429f5fc0f3d0a637
宝龙 <35037130+huanghuikang@users.noreply.github.com>
2018-11-27 22:48:05 +0800

    Update six.sql

--- SQLServer/six.sql
+++ SQLServer/six.sql
@@ -1,1 +1,15 @@
+USE [ekp]
+GO
+/****** Object:  StoredProcedure [dbo].[tongjiTFCLjianshu]    Script Date: 2018/11/14 14:46:45 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
+ALTER PROCEDURE [dbo].[tongjiTFCLjianshu]
+
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+	--创建表用于对超时完成，超时末完成和按时完成统计
